4b098a20ce4f8d38c51f1999ce3005e2d5b3bb63
Aaratrika srivastava <preeti.srivastava863@gmail.com>
2026-02-11 09:20:34 +0530

    Add files via upload

---
+++ 10.1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    scanf("%s", s);
+
+    int left = 0;
+    int right = strlen(s) - 1;
+    int isPalindrome = 1;  // assume true
+
+    // Two-pointer comparison
+    while (left < right) {
+        if (s[left] != s[right]) {
+            isPalindrome = 0;  // not palindrome
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (isPalindrome)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}

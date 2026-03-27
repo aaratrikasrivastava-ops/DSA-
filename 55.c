2dccd78818396facf40c6b174cc35a2f3b4bfe3c
Aaratrika srivastava <preeti.srivastava863@gmail.com>
2026-03-27 08:43:49 +0530

    Add files via upload

---
+++ 55.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+struct Queue {
+    struct Node* arr[1000];
+    int front, rear;
+};
+void initQueue(struct Queue* q) {
+    q->front = q->rear = 0;
+}
+
+int isEmpty(struct Queue* q) {
+    return q->front == q->rear;
+}
+
+void enqueue(struct Queue* q, struct Node* node) {
+    q->arr[q->rear++] = node;
+}
+
+struct Node* dequeue(struct Queue* q) {
+    return q->arr[q->front++];
+}
+void rightView(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    struct Queue q;
+    initQueue(&q);
+
+    enqueue(&q, root);
+
+    while (!isEmpty(&q)) {
+        int size = q.rear - q.front;
+
+        for (int i = 0; i < size; i++) {
+            struct Node* temp = dequeue(&q);
+            if (i == size - 1) {
+                printf("%d ", temp->data);
+            }
+
+            if (temp->left)
+                enqueue(&q, temp->left);
+            if (temp->right)
+                enqueue(&q, temp->right);
+        }
+    }
+}
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = newNode(arr[0]);
+    struct Queue q;
+    initQueue(&q);
+    enqueue(&q, root);
+
+    int i = 1;
+    while (i < n) {
+        struct Node* current = dequeue(&q);
+        if (arr[i] != -1) {
+            current->left = newNode(arr[i]);
+            enqueue(&q, current->left);
+        }
+        i++;
+        if (i < n && arr[i] != -1) {
+            current->right = newNode(arr[i]);
+            enqueue(&q, current->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+
+    rightView(root);
+
+    return 0;
+}

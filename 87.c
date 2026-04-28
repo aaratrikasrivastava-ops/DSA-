153acbe841466a8b72c7258005f7d2018bd2a3cd
Aaratrika srivastava <preeti.srivastava863@gmail.com>
2026-04-28 08:27:22 +0530

    Add files via upload

---
+++ 87.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int left = 0, right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result == -1)
+        printf("Element not found");
+    else
+        printf("%d", result);
+
+    return 0;
+}
